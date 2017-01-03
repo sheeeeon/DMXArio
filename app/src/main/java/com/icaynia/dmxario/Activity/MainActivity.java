55eb24851ebd53331abf2601040e0d9ef89ad465
icaynia <icaynia@icaynia.com>
2017-01-03 22:51:37 +0900

    activity initialize : profile

--- app/src/main/java/com/icaynia/dmxario/Activity/MainActivity.java
+++ app/src/main/java/com/icaynia/dmxario/Activity/MainActivity.java
@@ -3,9 +3,11 @@ package com.icaynia.dmxario.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +17,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 import com.icaynia.dmxario.Fragment.DmxprofileFragment;
 import com.icaynia.dmxario.Fragment.ProjectFragment;
@@ -22,14 +25,14 @@ import com.icaynia.dmxario.R;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
@@ -37,12 +40,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        View hView = navigationView.getHeaderView(0);
+        LinearLayout account = (LinearLayout) hView.findViewById(R.id.nav_account);
+
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onProfileActivity();
+                drawer.closeDrawer(GravityCompat.START);
+            }
+        });
+
         switchFragment(new ProjectFragment());
+
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -78,16 +92,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_project) {
-            // Handle the camera action
-        } else if (id == R.id.nav_dmxprofile) {
-
-        } else if (id == R.id.nav_message) {
-
-        } else if (id == R.id.nav_manage) {
-
-        }
-
         switch (item.getItemId()) {
             case R.id.nav_project:
                 switchFragment(new ProjectFragment());
@@ -105,6 +109,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
             case R.id.nav_manage:
                 break;
+            case R.id.nav_view:
+                onProfileActivity();
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -117,5 +124,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
         fragmentTransaction.replace(R.id.layout_fragment, fragment);
         fragmentTransaction.commit();
+    }
+
+    private void onProfileActivity() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
