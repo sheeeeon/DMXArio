1288a50e2bb34ba004c4bb65db19c96d050db9eb
icaynia <icaynia@icaynia.com>
2017-01-03 21:37:45 +0900

    card initialize : projectcard

---
+++ app/src/main/java/com/icaynia/dmxario/Model/ProjectCard.java
@@ -0,0 +1,20 @@
+package com.icaynia.dmxario.Model;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by icaynia on 2017. 1. 3..
+ */
+
+public class ProjectCard extends LinearLayout {
+
+    public ProjectCard(Context context) {
+        super(context);
+    }
+    
+    public ProjectCard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
