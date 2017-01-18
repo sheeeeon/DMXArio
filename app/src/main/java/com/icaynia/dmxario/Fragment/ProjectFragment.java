00773cb63cf009e6a9c82dc8f509d6c779ef822b
icaynia <icaynia@icaynia.com>
2017-01-18 16:21:17 +0900

    Improve the layout margin of card

--- app/src/main/java/com/icaynia/dmxario/Fragment/ProjectFragment.java
+++ app/src/main/java/com/icaynia/dmxario/Fragment/ProjectFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+
 import com.icaynia.dmxario.Activity.MainActivity;
 import com.icaynia.dmxario.R;
 import com.icaynia.dmxario.View.CardView;
