985a7d6bcfb0a74eebecad359e31f181d8ce40e8
icaynia <icaynia@icaynia.com>
2017-01-19 17:50:36 +0900

    Add signup activity

---
+++ app/src/main/java/com/icaynia/dmxario/Activity/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.icaynia.dmxario.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by icaynia on 19/01/2017.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
