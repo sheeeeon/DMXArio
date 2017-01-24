192b27cbb1f18cfa7f293a7a3dd3638638b9b154
icaynia <icaynia@icaynia.com>
2017-01-25 03:05:14 +0900

    Add a FollowManager class

---
+++ app/src/main/java/com/icaynia/dmxario/Data/FollowManager.java
@@ -0,0 +1,29 @@
+package com.icaynia.dmxario.Data;
+
+/**
+ * Created by icaynia on 25/01/2017.
+ */
+
+public class FollowManager {
+    public FollowManager() {
+
+    }
+
+//    나를 팔로우 하는 사람 get Follower.
+    public void getFollower() {
+
+    }
+
+    public int getFollowerRows() {
+
+    }
+
+//    내가 팔로우 하는 사람
+    public void getFollowing() {
+
+    }
+
+    public int getFollowingRows() {
+        return 50;
+    }
+}
