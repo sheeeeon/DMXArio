2bfc60573b52ae26fe02720d393b1813a783868e
icaynia <icaynia@icaynia.com>
2017-02-07 22:04:28 +0900

    Add search view

--- app/src/main/java/com/icaynia/dmxario/Layout/Fragment/FriendsFragment.java
+++ app/src/main/java/com/icaynia/dmxario/Layout/Fragment/FriendsFragment.java
@@ -1,6 +1,9 @@
 package com.icaynia.dmxario.Layout.Fragment;
 
 import android.app.Fragment;
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,10 +12,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v7.widget.SearchView;
 
 import com.icaynia.dmxario.Layout.Activity.MainActivity;
 import com.icaynia.dmxario.Global;
 import com.icaynia.dmxario.R;
+
+import static android.support.v4.view.MenuItemCompat.getActionView;
 
 /**
  * Created by icaynia on 2017. 1. 13..
@@ -21,6 +27,8 @@ import com.icaynia.dmxario.R;
 public class FriendsFragment extends Fragment {
     private View v;
     private Global global;
+
+    private SearchView searchView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_friends, container, false);
@@ -44,16 +52,23 @@ public class FriendsFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.main_fragment_project, menu);  // Use filter.xml from step 1
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.main_fragment_friend, menu);  // Use filter.xml from step 1
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView = (SearchView) getActionView(item);
+        searchView.setSubmitButtonEnabled(true);
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_add_project) {
-
+        if (id == R.id.action_search) {
             return true;
         }
+
+
 
         return super.onOptionsItemSelected(item);
     }
